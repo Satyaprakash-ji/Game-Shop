258c4de00ae6bb4757805b138891ffc139e7ed11
Satyaprakash-ji <satyaprakash91407@gmail.com>
2025-05-31 17:54:15 +0530

    fix nav

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -8,6 +8,8 @@ import { IoCartOutline } from "react-icons/io5";
 import { IoSearch } from "react-icons/io5";
 import { Tooltip as ReactTooltip } from 'react-tooltip'
 import { AuthContext } from "../../contexts/AuthContext";
+import { CartContext } from "../../contexts/CartContext";
+import { WishlistContext } from "../../contexts/WishlistContext";
 
 const NavigationBar = () => {
 
@@ -15,6 +17,8 @@ const NavigationBar = () => {
     const { filterDispatch } = useContext(FilterDispatchContext)
     const { loginData, logOut } = useContext(AuthContext)
     const navigate = useNavigate();
+    const { wishlist } = useContext(WishlistContext)
+    const { cart } = useContext(CartContext);
 
     const handleSearchFilterChange = (event) => {
         filterDispatch({
@@ -58,9 +62,16 @@ const NavigationBar = () => {
             </div>
             <div className="right-part">
                 { loginData.isLoggedIn ? <p className="navbar-link"><span onClick={() => navigate("/profile")}>Profile</span>/<span onClick={() => logOut()}>LogOut</span></p> : <p className="navbar-link"><span onClick={() => navigate("/login")}>Login</span>/<span onClick={() => navigate("/register")}>Register</span></p> }
-                <IoMdHeartEmpty className="wishlist-icon" onClick={() => navigate("/wishlist")}/>
+                <div className="wishlist-box">
+                    <IoMdHeartEmpty size={24} className="wishlist-icon" onClick={() => navigate("/wishlist")}/>
+                    {wishlist.wishlistData.length > 0 && <span className="wishlist-total">{wishlist.wishlistData.length}</span>}
+                    
+                </div>
                 <ReactTooltip />
-                <IoCartOutline  title="Cart" className="shoping-cart-icon" onClick={() => navigate("/cart")}/>
+                <div className="cart-box">
+                    <IoCartOutline size={24}  title="Cart" className="shoping-cart-icon" onClick={() => navigate("/cart")}/>
+                    {cart.cartData.length > 0 && <span className="cart-total">{cart.cartData.length}</span>}
+                </div>
             </div>
         </div>
     )
