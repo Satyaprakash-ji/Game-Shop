0dba4b1a4798269ce7d21b5e2851ebe31efcae7d
Satyaprakash-ji <satyaprakash91407@gmail.com>
2025-07-23 09:41:55 +0530

    Add Backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: ['http://localhost:3000', 'https://game-shop-ten-olive.vercel.app'],
         changeOrigin: true,
         secure: false,
       },
